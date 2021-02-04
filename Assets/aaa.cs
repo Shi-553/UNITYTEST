da01c564d957c28b47ed2db44c6f55911e410120
sisyamo2236 <sisyamo2236@gmail.com>
2021-02-04 11:05:57 +0900

    aaaの追加

--- Assets/aaa.cs
+++ Assets/aaa.cs
@@ -2,17 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class teest : MonoBehaviour
+public class aaa : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-//iosgdjgsihgshighdsioihgiosdhihioajgosogjpogjsfg
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-//hiohdsiogioshgiohdighighsdiiodgoisdhihidsgidohgdssdhgsd
+        
     }
 }
