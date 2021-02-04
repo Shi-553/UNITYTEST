da56afa17f864e5e9aa6e902520a7318a03e6b2e
sisyamo2236 <sisyamo2236@gmail.com>
2021-02-04 12:07:26 +0900

    回転数の変更

--- Assets/rotation.cs
+++ Assets/rotation.cs
@@ -13,6 +13,6 @@ public class rotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(0, 90, 0));
+        transform.Rotate(new Vector3(10, 90, 50));
     }
 }
