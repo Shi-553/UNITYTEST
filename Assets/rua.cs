5c68f6e7ff2d03e158336641c7771463312b56c8
HALAT12G05 <74965607+HALAT12G05@users.noreply.github.com>
2021-02-04 11:54:51 +0900

    ruaが変えた

---
+++ Assets/rua.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rua : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int rua = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rua++;
+        if (rua==10)
+        {
+            rua = 0;
+        }
+    }
+}
